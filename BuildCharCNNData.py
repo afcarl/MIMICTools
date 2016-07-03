61a0057110b1b5e171119ea084bebe942f799322
Ankit Vani <a@nevitus.org>
2016-07-03 03:56:32 -0400

    Use a limited vocab

--- BuildCharCNNData.py
+++ BuildCharCNNData.py
@@ -23,7 +23,16 @@ notes_files = [f for f in utils.subset([f for f in notes_files
                                         train_size + valid_size + test_size)]
 random.shuffle(notes_files)
 
-fix_re = re.compile(r'[^a-z0-9/?.,-]+')
+fix_re = re.compile(r"[^a-z0-9/'?.,-]+")
+num_re = re.compile(r'[0-9]+')
+dash_re = re.compile(r'-+')
+
+def fix_word(word):
+    word = word.lower()
+    word = fix_re.sub('-', word).strip('-')
+    word = num_re.sub('#', word)
+    word = dash_re.sub('-', word)
+    return word
 
 files = {}
 files['train'] = notes_files[:train_size]
@@ -31,20 +40,29 @@ notes_files = notes_files[train_size:]
 files['valid'] = notes_files[:valid_size]
 files['test'] = notes_files[valid_size:]
 
+with open('vocab_fd.pk', 'rb') as f:
+    vocab_fd = pickle.load(f)
+vocab = set(k for k,v in vocab_fd.most_common(25000))
+
 def prepare_dataset(out):
     with open(out + '.txt', 'w') as f:
-        for _, raw_text in utils.mimic_data(files[out], replace_anon='*unk*',
+        for _, raw_text in utils.mimic_data(files[out], replace_anon='//unk//',
                                             verbose=True):
             sentences = nltk.sent_tokenize(raw_text)
             for sent in sentences:
-                words = [fix_re.sub('-', w.lower()).strip('-')
-                                    for w in nltk.word_tokenize(sent)
+                words = [fix_word(w) for w in nltk.word_tokenize(sent)
                                         if any(c.isalpha() or c.isdigit()
                                             for c in w)]
-                words = [w for w in words if w] # TODO: include only top 15000 words
-                line = ' '.join(words)
+                finalwords = []
+                for word in words:
+                    if not word: continue
+                    if word in vocab:
+                        finalwords.append(word)
+                    else:
+                        finalwords.append('<unk>')
+                line = ' '.join(finalwords)
                 if line:
-                    print >> f, ' ' + line.replace('*unk*', '<unk>')
+                    print >> f, ' ' + line.replace('//unk//', '<unk>')
 
 p = Pool(3)
 p.map(prepare_dataset, ['train', 'valid', 'test'])
